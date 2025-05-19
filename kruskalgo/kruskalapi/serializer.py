e2a73007fd750b980dff4d7054d6afe875755eaf
JamalMedamine <med.amine.jamal3@gmail.com>
2025-05-19 20:28:23 +0200

    auth & user crud

---
+++ kruskalgo/kruskalapi/serializer.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().create(validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
+    
--- kruskalgo/kruskalapi/urls.py
+++ kruskalgo/kruskalapi/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path , include
+from . import views 
 
 urlpatterns = [
-
+    path('register/', views.Register,name="register"),
+    path('login/', views.Login,name="login"),
+    path('users/', views.get_user_list, name='user_list'),
+    path('user/<int:pk>/', views.user_detail, name='user_detail'),
 ]
